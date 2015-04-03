f6538d2891e98e24d2f7dae901c456ba3b119e4f
Ariel Tubaltsev <tubaltzev@gmail.com>
2015-04-02 18:02:40 -0700

    mysql package and service

--- puppet/manifests/packages.pp
+++ puppet/manifests/packages.pp
@@ -1,4 +1,20 @@
-# packages to install
+# execute 'apt-get update'
+exec { 'apt-update':                    # exec resource named 'apt-update'
+  command => '/usr/bin/apt-get update'  # command this resource will run
+}
+
+# install packages
 package { "nodejs":
-    ensure => "installed"
+  require => Exec['apt-update'],
+  ensure => "installed"
+}
+
+package { "mysql-server":
+  require => Exec['apt-update'],
+  ensure => "installed"
+}
+
+# start services
+service { 'mysql':
+  ensure => running,
 }
